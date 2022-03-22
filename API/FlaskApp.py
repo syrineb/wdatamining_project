9b5a1f9c4e817e5d5c1a72e7e40cc18e69fa9d50
Syrine <syrine.bouchelleghem@gmail.com>
2022-03-22 20:17:40 +0100

    update

--- API/FlaskApp.py
+++ API/FlaskApp.py
@@ -8,9 +8,23 @@ app=Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('map.html')
-@app.route('/velo')
-def index2():
-	return render_template('velo.html')
 
+@app.route('/normal')
+def index2():
+	return render_template('normal.html')
+
+@app.route('/deuxroues')
+def index3():
+	return render_template('deuxroues.html')
+
+@app.route('/squares')
+def index4():
+	return render_template('squares.html')
+
+@app.route('/terrains')
+def index5():
+	return render_template('terrains.html')
+
+    
 if __name__ == "__main__":
     app.run(port=5002, debug=True, threaded=True)
